b7cf25c48d92f7718e381e3630fec07de989333f
Naga Srinivasa <cloudcomputingchannel@gmail.com>
2025-02-28 12:11:04 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -7,3 +7,7 @@ resource "aws_s3_bucket" "ccit_s3" {
   bucket = "svinfobucket"
   tags = { Name = "Terraform" }
 }
+resource "aws_s3_bucket" "ccit_s33" {
+  bucket = "svinfobucket1"
+  tags = { Name = "Terraform" }
+}
